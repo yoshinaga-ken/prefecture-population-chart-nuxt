21a63823ad857b43962c27cfac7f5724e367bd6c
yottushy75 <yottushy75@gmail.com>
2020-09-27 20:06:23 +0900

    * チャートのデータをRESAS(地域経済分析システム) API から取得するように修正 API:「都道府県一覧」と「各都道府県の総人口」

--- components/PrefChart.vue
+++ components/PrefChart.vue
@@ -3,15 +3,76 @@
 </template>
 
 <script>
-// import _ from 'lodash'
+import _ from 'lodash'
 import Highcharts from 'highcharts'
+
+import axios from 'axios'
+// RESAS(地域経済分析システム) API
+// API: https://opendata.resas-portal.go.jp/docs/api/v1/index.html
+const X_API_KEY = '3BKB29A7OA7kXnQ55wyn8Y3lKmGMrT6SnHH24ASv'
+const axiosGetFromRESAS = (url) =>
+  axios.get(url, { headers: { 'X-API-KEY': X_API_KEY } })
+const API_PREFECTURES = 'https://opendata.resas-portal.go.jp/api/v1/prefectures'
+const API_PREF_POPULATION =
+  'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear'
+
+const POPULATION_TYPE = ['total', 'yang', 'working', 'aged']
 
 export default {
   name: 'PrefChart',
   mounted() {
-    this.createChart()
+    this.getPrefData('total').then((prefData) => {
+      this.createChart(prefData.series, prefData.pointStart)
+    })
   },
   methods: {
+    getPrefData(poplationType) {
+      const ret = {
+        prefectures: {},
+        series: [],
+        pointStart: Number.MAX_SAFE_INTEGER,
+      }
+
+      // 都道府県のデータを取得
+      return axiosGetFromRESAS(API_PREFECTURES).then((response) => {
+        for (let i = 0; i < response.data.result.length; i++) {
+          ret.prefectures[response.data.result[i].prefCode] =
+            response.data.result[i].prefName
+        }
+
+        const prefCodes = response.data.result.map((d) => d.prefCode)
+
+        // 全ての都道府県の総人口のデータを取得 (非同期)
+        return Promise.all(
+          prefCodes.map((prefCode) =>
+            axiosGetFromRESAS(
+              API_PREF_POPULATION + '?cityCode=-&prefCode=' + prefCode
+            )
+          )
+        ).then((responses) => {
+          const idx = POPULATION_TYPE.indexOf(poplationType)
+          for (let i = 0; i < responses.length; i++) {
+            const popTotal = responses[i].data.result.data[idx].data
+            const years = popTotal.map((d) => d.year)
+            const values = popTotal.map((d) => d.value)
+
+            // 全ての都道府県で最小の年度を取得
+            const min = _.min(years)
+            if (min < ret.pointStart) {
+              ret.pointStart = min
+            }
+
+            const item = {
+              name: ret.prefectures[prefCodes[i]],
+              data: values,
+            }
+            ret.series.push(item)
+          }
+
+          return Promise.resolve(ret)
+        })
+      })
+    },
     createChart(series, pointStart) {
       Highcharts.setOptions({
         lang: {
@@ -68,25 +129,12 @@ export default {
             label: {
               connectorAllowed: false,
             },
-            pointStart: 1980,
+            pointStart,
             pointInterval: 5,
           },
         },
 
-        series: [
-          {
-            name: '東京',
-            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
-          },
-          {
-            name: '大阪',
-            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
-          },
-          {
-            name: '福岡',
-            data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
-          },
-        ],
+        series,
 
         responsive: {
           rules: [
